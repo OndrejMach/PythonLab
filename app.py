da0d71c6e142f9545bebffe21df3ca47e8998d52
machaceko <machaceko@centrum.cz>
2017-10-11 16:52:24 +0200

    zxczxc

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-name = input("What's your name bro? ")
-print("Nice to meet you " + name + "!")
-age = input("Your age? ")
-print("So, you are already " + str(age) + " years old, " + name + " bro!")
+
+print("Nice to meet you ""!")
+
+print("So, you are already  bro!")
 
